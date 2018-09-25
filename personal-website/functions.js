18728932cdb99510efc2c19103664fe2854c761f
Nikzt <bence.linder@gmail.com>
2018-09-25 16:55:35 -0400

    youtube videos scale with screen size

--- personal-website/functions.js
+++ personal-website/functions.js
@@ -3,6 +3,7 @@ function switchCollapse(id) {
 	var indicator = element.childNodes[0];
 	if (element.getAttribute("aria-expanded") === "false") {
 		indicator.innerHTML = "&minus;";	
+		window.scrollTo(0, element.getBoundingClientRect().top);
 	}	else {
 		indicator.innerHTML = "+";	
 	}
